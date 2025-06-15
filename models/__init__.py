69eb4a54a60dc2b87952cb519748cd59883f7977
Tom E Harnes <protoxcode@gmail.com>
2025-06-15 14:47:32 +0200

    Windows DLL
    The needed Windows DLL files so far.
    
    Took 7 minutes

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+import os
+import platform
+
+if platform.system() == "Windows":
+    dll_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "runtime", "win_x64"))
+    if hasattr(os, "add_dll_directory"):
+        os.add_dll_directory(dll_path)
+    else:
+        os.environ["PATH"] = dll_path + os.pathsep + os.environ["PATH"]
