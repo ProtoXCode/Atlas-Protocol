cad29fdab8d88cbed8c8a2b656899a60d7e88eeb
Tom E Harnes <protoxcode@gmail.com>
2025-08-09 16:12:43 +0200

    Update main.py
    Launches on Linux Fedora now.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
 import sys
+import os
+
+# --- force Qt to use X11 when running on Wayland, works on Fedora ---
+if os.name == 'posix':
+    if os.environ.get('XDG_SESSION_TYPE', '').lower() == 'wayland':
+        os.environ.setdefault('QT_QPA_PLATFORM', 'xcb')
 
 from PySide6.QtWidgets import QApplication
 import qt_material
 
 from gui.main_window import MainWindow
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     qt_material.apply_stylesheet(app, theme='dark_blue.xml')
 
